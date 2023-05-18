6dcc72cc65afbf5d3582dcda4781745dfea909aa
Golam Sarwar <gsshaun@gmail.com>
2023-05-18 16:26:23 +0900

    movie rest api

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,41 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'title', type: 'varchar', length: 255 })
+  title: string;
+
+  @Column({ name: 'imdb_id', type: 'varchar', length: 20, nullable: true })
+  imdbId: string;
+
+  @Column({ name: 'poster', type: 'varchar', length: 255, nullable: true })
+  poster: string;
+
+  @Column({ name: 'year'})
+  year: number;
+
+  @Column({ name: 'type', type: 'varchar', length: 20, nullable: true })
+  type: string;
+
+  @Column({ name: 'director', type: 'varchar', length: 20, nullable: true })
+  director: string;
+
+  @Column({ name: 'plot', type: 'varchar', length: 255, nullable: true })
+  plot: string;
+
+  @Column({ name: 'actors', type: 'varchar', length: 255, nullable: true })
+  actors: string[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class MoviesService {
+  constructor(
+    @InjectRepository(Movie)
+    private movieRepository: Repository<Movie>,
+  ) {}
+
+  async createMovie(createMovieDto: CreateMovieDto) {
+    const newMovie = this.movieRepository.create(createMovieDto);
+    return await this.movieRepository.save(newMovie);
+  }
+
+  async findAllMovies(): Promise<Movie[]> {
+    return await this.movieRepository.find();
+  }
+
+  async getMovieById(id: number) {
+    const movie = await this.movieRepository.findOne({ where: { id: id } });
+    if (!movie) {
+      throw new NotFoundException('Movie not found');
+    }
+    return movie;
+  }
+
+  async updateMovie(
+    id: number,
+    updateMovieDto: UpdateMovieDto,
+  ): Promise<Movie> {
+    const movie = await this.movieRepository.findOne({ where: { id: id } });
+    if (!movie) {
+      throw new NotFoundException('Movie not found');
+    }
+    Object.assign(movie, updateMovieDto);
+    return await this.movieRepository.save(movie);
+  }
+
+  async removeMovie(id: number) {
+    const movie = await this.movieRepository.findOne({ where: { id: id } });
+    if (!movie) {
+      throw new NotFoundException('Movie not found');
+    }
+    await this.movieRepository.remove(movie);
+    return `Movie with ID ${id} is removed`;
+  }
+}
